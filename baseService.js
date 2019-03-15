8271796b455060f86e02c7fb49828f2666ea83af
Joan <setoba1192@gmail.com>
2019-03-15 10:43:30 -0500

    Actualizacion Token

--- baseService.js
+++ baseService.js
@@ -9,18 +9,18 @@
       }, {
          getLeads : {
             method : 'GET',
-            headers: { 'Authorization': 'Bearer 00D1U000001531A!AQwAQL9WQlmOI__2jDvJKcSNxvUcMQciXZuu3VNrrsJEUnwIVbScFKbcdTEhvyxwHha70e9wTPz0EBcNB7g8dYDO1BUDXktj' },
+            headers: { 'Authorization': 'Bearer 00D1U000001531A!AQwAQOgSHqJnOPk80dGgaLwah7BjfKtEmFgygvrq754jFk.dZKpjqvKi0VCzQ3dR5qSEiHMLARTJoaOQHeUfa9I4xoSEZ5Oc' },
             url : 'https://na85.salesforce.com/services/apexrest/Leads'
          },
          getItemsByLead : {
             method : 'GET',
-            headers: { 'Authorization': 'Bearer 00D1U000001531A!AQwAQL9WQlmOI__2jDvJKcSNxvUcMQciXZuu3VNrrsJEUnwIVbScFKbcdTEhvyxwHha70e9wTPz0EBcNB7g8dYDO1BUDXktj' },
+            headers: { 'Authorization': 'Bearer 00D1U000001531A!AQwAQOgSHqJnOPk80dGgaLwah7BjfKtEmFgygvrq754jFk.dZKpjqvKi0VCzQ3dR5qSEiHMLARTJoaOQHeUfa9I4xoSEZ5Oc' },
             url : 'https://na85.salesforce.com/services/apexrest/item',
           
          },
          createItem : {
             method : 'POST',
-            headers: { 'Authorization': 'Bearer 00D1U000001531A!AQwAQL9WQlmOI__2jDvJKcSNxvUcMQciXZuu3VNrrsJEUnwIVbScFKbcdTEhvyxwHha70e9wTPz0EBcNB7g8dYDO1BUDXktj' },
+            headers: { 'Authorization': 'Bearer 00D1U000001531A!AQwAQOgSHqJnOPk80dGgaLwah7BjfKtEmFgygvrq754jFk.dZKpjqvKi0VCzQ3dR5qSEiHMLARTJoaOQHeUfa9I4xoSEZ5Oc' },
             url : 'https://na85.salesforce.com/services/apexrest/item',
           
          }
